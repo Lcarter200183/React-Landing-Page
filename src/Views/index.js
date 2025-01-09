b50402bb86f1407d2ac4f99108b382404e672b45
Lydia <Lcarter200183@yahoo.com>
2025-01-09 16:00:41 -0600

    added info

--- src/Views/index.js
+++ src/Views/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client.js";
 import React from "react.js";
-import { ReactLandingPage } from "./Views/ReactLandingPage.js";
+
 
 const root = createRoot(window.bodyTag);
  root.render(
